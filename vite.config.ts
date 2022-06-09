1d1ec0de1b13fd05efa46122a72a189de741df20
xiaoshunshi <song159769@163.com>
2022-06-09 11:25:51 +0800

    配置base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/vite-shop-admin',
-  publicDir:'/vite-shop-admin',
+  base:'/vite-shop-admin/',
   plugins: [vue()]
 })
