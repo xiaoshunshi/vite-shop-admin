17082a4dbc91290901ec9ff52588afe2319c1a92
xiaoshunshi <song159769@163.com>
2022-06-09 11:33:49 +0800

    发布到git page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+console.log(process.env.NODE_ENV)
+const isPro = process.env.NODE_ENV === 'production'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/vite-shop-admin/',
+  base:isPro ? '/vite-shop-admin/':'/',
   plugins: [vue()]
 })
