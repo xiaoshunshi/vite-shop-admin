0206b07992830f786c6e2d2b87f5a2847c839c9c
xiaoshunshi <song159769@163.com>
2022-06-09 17:14:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello xiaoshunshi22222" />
+  <HelloWorld msg="Hello xiaoshunshi333" />
 </template>
 
 <style>
